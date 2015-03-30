a98e64bd3a068b2a775313a3a303477f0a69923b
artur <akhalikoff@gmail.com>
2015-03-30 22:51:03 +0300

    Strings and StringBuffers

--- part1/src/java/StringBuffers.java
+++ part1/src/java/StringBuffers.java
@@ -1,6 +1,6 @@
 class StringBuffers {
 	public static void main(String[] args) {
-		StringBuffer sb = new StringBuffer("Hello world");
+		StringBuffer sb = new StringBuffer("If music be the food of love, play on.");
 
 		// Ensures that the capacity of the buffer is at least equal to the specified minimum.
 		sb.ensureCapacity(10);
@@ -14,6 +14,22 @@ class StringBuffers {
 		sb.setLength(2);
 		System.out.println(sb.toString()); // --> He
 
-		sb.clear();
+		// No such method!
+		//sb.clear();
+
+		// How to clear StringBuffer ?
+		sb.delete(0, sb.length());
+		System.out.println("sb = " + sb.toString());
+
+		// Append returns modified StringBuffer object
+		// so you can make chaining:
+		sb
+			.append("Love all, trust a few, ")
+			.append("do wrong to none.");
+		System.out.println(sb);
+
+		// append supports all kind of arguments:
+		// boolean, char, char array, char array with start index and offset,
+		// double, float, int, long, Object, String, StringBuffer			
 	}
 }
--- part1/src/java/Strings.java
+++ part1/src/java/Strings.java
@@ -1,12 +1,37 @@
 class Strings {
 	public static void main(String[] args) {
+
+		// printing null
+		System.out.println("On next line should be null:");
+		String str = null;
+		System.out.println(str);
+
+		// Below will throw error: reference to println is ambiguous
+		// System.out.println(null);
 		
 		testReplace();
 
 		testTrim();
+
+		testSubstring();
+	}
+
+	static void testSubstring() {
+		System.out.println("==== Substring ====");
+		
+		//          0123456
+		String s = "minimum";
+
+		// substring(beginIndex)
+		System.out.println(s.substring(4)); // mum
+		
+		// substring(beginIndex, endIndex) - beginIndex included, endIndex - not included 
+		System.out.println(s.substring(1, 4)); // ini
 	}
 
 	static void testReplace() {
+		System.out.println("==== Replace ====");
+
 		// true
 		System.out.println("String" == "String");
 
@@ -25,6 +50,8 @@ class Strings {
 	}
 
 	static void testTrim() {
+		System.out.println("==== Trim ====");
+
 		String blank = " ";
 		String line = blank + "hello" + blank + blank;
 		line.concat("world"); // tricky! remember that String is immutable !
